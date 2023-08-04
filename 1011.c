089f703844e776c1570b6d2a5f224914db027599
Tanjina Akter Nisa <140683300+tanjinanisa86@users.noreply.github.com>
2023-08-04 21:31:20 +0600

    Add files via upload

---
+++ 1011.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double R,V;
+	double P=3.14159;
+	scanf("%lf",&R);
+	V=(4/3.0)*P*pow(R,3);
+	printf("VOLUME = %.3lf\n",V);
+
+	
+return 0;
+	
+}
